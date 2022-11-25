24aaf143e988a3e0c0bb20ba6053b478676873d7
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-11-25 16:33:49 -0600

    added an asset graph to show lineage

--- my-dagster-project/cereal.py
+++ my-dagster-project/cereal.py
@@ -10,35 +10,3 @@ def cereals():
 
     return cereal_rows
 
-@asset
-def nabisco_cereals(cereals):
-    """Cereals manufactured by Nabisco"""
-    return [row for row in cereals if row["mfr"] == "N"]
-
-#from dagster import materialize
-
-#if __name__ == "__main__":
-#    materialize([cereals])
-
-@asset
-def cereal_protein_fractions(cereals):
-    """
-    For each cereal, records its protein content as a fraction of its total mass.
-    """
-    result = {}
-    for cereal in cereals:
-        total_grams = float(cereal["weight"]) * 28.35
-        result[cereal["name"]] = float(cereal["protein"]) / total_grams
-
-    return result
-
-
-@asset
-def highest_protein_nabisco_cereal(nabisco_cereals, cereal_protein_fractions):
-    """
-    The name of the nabisco cereal that has the highest protein content.
-    """
-    sorted_by_protein = sorted(
-        nabisco_cereals, key=lambda cereal: cereal_protein_fractions[cereal["name"]]
-    )
-    return sorted_by_protein[-1]["name"]
